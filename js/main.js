8ec6654be070f12ca57340978902c8e2585f0ef5
Lain Drion <igencap@gmail.com>
2022-09-06 18:10:46 +0500

    Made some changes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let square = document.querySelector('.square');
+
+function draw(a,b){
+    if(document.onclick(square)){
+        return console.log('wow');
+    } else{
+        return console.log('Not wow');
+    }
+}
